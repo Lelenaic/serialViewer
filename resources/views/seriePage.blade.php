39f4928c3277119431f8e679e79a60df2cc54c01
FremontJoshua <fremont.joshua85@gmail.com>
2018-02-15 16:02:47 +0100

    tests

--- resources/views/seriePage.blade.php
+++ resources/views/seriePage.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">{{ $serie['nom'] }}</div>
-
                 <div class="panel-body">
                   <img src="{{ $serie['urlImage'] }}"></img>
                   {{ $serie['resume'] }}
--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -2,15 +2,23 @@
 
 @section('content')
 <div class="container">
+  <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+          <div class="panel panel-default">
+              <div class="panel-heading">Thèmes</div>
+              <div class="panel-body">
+
+              </div>
+          </div>
+      </div>
+  </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Toutes les séries</div>
-
                 <div class="panel-body">
                   <ul>
                     @foreach ($series as $serie)
-
                       <li><a href="{{ url('series/'.$serie['id']) }}">{{ $serie['nom'] }}</a></li>
                     @endforeach
                   </ul>
